7215cec622127f2f908c9b04bda06fcb7bb68cfa
nico239677 <nicolasmai0421@gmail.com>
2020-06-15 23:59:30 +0300

    vraie version uploadée

--- web_scraping.py
+++ web_scraping.py
@@ -3,34 +3,21 @@ from bs4 import BeautifulSoup
 
 # Links (possible de tout réunir dans une liste cf exemple ITC)
 link = 'https://www.basketball-reference.com/'
-link_results = 'https://www.basketball-reference.com/boxscores/'
+r = requests.get(link)
 
-# Assess if connection works
-try:
-    r1 = requests.get(link)
-except ConnectionError as e:
-    print('Connection error')
-    raise
+soup = BeautifulSoup(r.content, 'lxml')
 
-# Working with response code
-r1.status_code == requests.codes.ok
-print(requests.codes['temporary_redirect'])
-print(requests.codes.teapot)
-print(requests.codes['o/'])
+# Get global stats
+print("RESULTS OF THE YEAR SO FAR")
+gros_tableau = soup.find("div", attrs={'class': "data_grid section_wrapper"})
+table_list = gros_tableau.find_all('div', attrs={'class':"table_wrapper"})
 
-# Working with headers
-resp = requests.head(link)
-print(resp.status_code, resp.text, resp.headers)
-print(r.encoding)
-
-# Create pools of proxies and headers and get the first ones (cf exo ITC)
-proxies_pool, headers_pool = create_pools()
-current_proxy = next(proxy_pool)
-current_headers = next(headers_pool)
-
-# START SCRAPING
-soup1 = BeautifulSoup(r1.content, 'html.parser')
-soup2 = BeautifulSoup(r2.content, 'html.parser')
-#print(soup.prettify())
-scores = soup1.find(class_="game_summary expanded nohover")
-scores = soup2.find(class_="table_wrapper")
+for table in table_list:
+    all_rows = table.find_all("tr", attrs={'class':'full_table'})
+    break
+for row in all_rows:
+    print(row.a['title'], ' : ', row.td.nextSibling.nextSibling.text, ' wins, ',
+          row.td.nextSibling.nextSibling.nextSibling.text, 'losses')
+    #print()
+# for row in all_rows.child:
+#     print(row)
