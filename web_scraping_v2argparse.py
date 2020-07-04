f9261320528a07ce22bb308712170d50cc69a5fb
nico239677 <nicolasmai0421@gmail.com>
2020-07-04 20:51:56 +0300

    Inserted to table for first time

--- web_scraping_v2argparse.py
+++ web_scraping_v2argparse.py
@@ -3,9 +3,14 @@ import argparse
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from sqlalchemy import create_engine
 from datetime import datetime
-today = datetime.today()
 
+with open('create_table.py', "rb") as source_file:
+    code = compile(source_file.read(), 'create_table.py', "exec")
+exec(code)
+
+# Parsing years of data to scrap
 parser = argparse.ArgumentParser()
 parser.add_argument('-first',
                     '--start_year',
@@ -15,7 +20,7 @@ parser.add_argument('-first',
 parser.add_argument('-last',
                     '--end_year',
                     type=int,
-                    default=today,
+                    default=datetime.today(),
                     help='choose last year of data to scrap')
 args = parser.parse_args()
 
@@ -25,6 +30,7 @@ def read_link(link):
     # Handles error if link is incorrect
     try:
         r = requests.get(link)
+        print('link: \n', link)
     except requests.exceptions.ConnectionError:
         print('URL not valid')
         sys.exit()
@@ -34,14 +40,14 @@ def read_link(link):
 
 def add_tag_link(global_list, string):
     if string in str(draft.find('a')):
-        element = draft.find('a').text if draft.find('a').text else "NaN"
+        element = draft.find('a').text if draft.find('a').text else None
         global_list.append(element)
         return
 
 
-def add_tag_text(global_list, string):
+def add_tag_text_in_tag(global_list, string):
     if string in str(draft):
-        element = draft.text if draft.text else 'NaN'
+        element = draft.text if draft.text else None
         # print('| number of games: ', number_games, end=" ")
         global_list.append(element)
 
@@ -51,6 +57,13 @@ main_link = 'https://www.basketball-reference.com/'
 # Getting the data of all drafts for a specific period of time
 FIRST_YEAR = args.start_year
 LAST_YEAR = args.end_year
+
+# Checking that last year comes after first year
+try:
+    assert (LAST_YEAR > FIRST_YEAR)
+except AssertionError:
+    print("Last year to scrap should be higher than first year")
+    sys.exit()
 
 for year in range(FIRST_YEAR, LAST_YEAR+1):
     link_draft = main_link + 'draft/NBA_' + str(year) + '.html'
@@ -63,137 +76,107 @@ for year in range(FIRST_YEAR, LAST_YEAR+1):
 
     draft_list = []
     for draft in body:
+        # Adding year
+        # Adding player info
         add_tag_link(draft_list, 'play-index')
-        # if 'play-index' in str(draft.find('a')):
-        #     index = draft.find('a').text if draft.find('a').text else "NaN"
-        #     # print(index, end=" ")
-        #     draft_list.append(index)
         add_tag_link(draft_list, 'players')
-        # if 'players' in str(draft.find('a')):
-        #     name_player = draft.find('a').text if draft.find('a').text else "NaN"
-        #     # print(name_player, end=" ")
-        #     draft_list.append(name_player)
-        add_tag_text(draft_list, 'data-stat="g"')
-        # if 'data-stat="g"' in str(draft):
-        #     number_games = draft.text if draft.text else 'NaN'
-        #     # print('| number of games: ', number_games, end=" ")
-        #     draft_list.append(number_games)
-        add_tag_text(draft_list, 'data-stat="mp"')
-        # if 'data-stat="mp"' in str(draft):
-        #     minutes_played = draft.text if draft.text else 'NaN'
-        #     #     # print('| total minutes played: ', minutes_played)
-        #     draft_list.append(minutes_played)
-        add_tag_text(draft_list, 'data-stat="pts"')
-        # if 'data-stat="pts"' in str(draft):
-        #     points = draft.text if draft.text else 'NaN'
-        #     #     # print('| assists per game: ', assists_per_game)
-        #     draft_list.append(points)
-        add_tag_text(draft_list, 'data-stat="trb"')
-        # if 'data-stat="trb"' in str(draft):
-        #     rebounds = draft.text if draft.text else 'NaN'
-        #     #     # print('| assists per game: ', assists_per_game)
-        #     draft_list.append(rebounds)
-        add_tag_text(draft_list, 'data-stat="ast"')
-        # if 'data-stat="ast"' in str(draft):
-        #     assists = draft.text if draft.text else 'NaN'
-        #     #     # print('| assists per game: ', assists_per_game)
-        #     draft_list.append(assists)
-        add_tag_text(draft_list, 'data-stat="mp_per_g"')
-        # if 'data-stat="mp_per_g"' in str(draft):
-        #     minutes_played_per_game = draft.text if draft.text else 'NaN'
-        #     # print('| minutes played per game: ', minutes_played_per_game, end=" ")
-        #     draft_list.append(minutes_played_per_game)
-        add_tag_text(draft_list, 'data-stat="pts_per_g"')
-        # if 'data-stat="pts_per_g"' in str(draft):
-        #     points_per_game = draft.text if draft.text else 'NaN'
-        #     #     # print('| points per games: ', points_per_game, end=" ")
-        #     draft_list.append(points_per_game)
-        add_tag_text(draft_list, 'data-stat="trb_per_g"')
-        # if 'data-stat="trb_per_g"' in str(draft):
-        #     rebounds_per_game = draft.text if draft.text else 'NaN'
-        #     #     # print('| rebounds per games: ', rebounds_per_game, end=" ")
-        #     draft_list.append(rebounds_per_game)
-        add_tag_text(draft_list, 'data-stat="ast_per_g"')
-        # if 'data-stat="ast_per_g"' in str(draft):
-        #     assists_per_game = draft.text if draft.text else 'NaN'
-        #     #     # print('| assists per game: ', assists_per_game)
-        #     draft_list.append(assists_per_game)
 
+        add_tag_text_in_tag(draft_list, 'data-stat="g"')
+        add_tag_text_in_tag(draft_list, 'data-stat="mp"')
+        add_tag_text_in_tag(draft_list, 'data-stat="pts"')
+
+        add_tag_text_in_tag(draft_list, 'data-stat="trb"')
+        add_tag_text_in_tag(draft_list, 'data-stat="ast"')
+        add_tag_text_in_tag(draft_list, 'data-stat="mp_per_g"')
+        add_tag_text_in_tag(draft_list, 'data-stat="pts_per_g"')
+        add_tag_text_in_tag(draft_list, 'data-stat="trb_per_g"')
+        add_tag_text_in_tag(draft_list, 'data-stat="ast_per_g"')
+        draft_list.append(year)
+
+    print('draft_list is ', draft_list)
     # Turning the list into list of lists
-    updated_draft_list = [draft_list[x:x + 11] for x in range(0, len(draft_list), 11)]
+    updated_draft_list = [draft_list[x:x + 12] for x in range(0, len(draft_list), 12)]
     name = 'list_draft_' + str(year) + '.csv'
 
-    # Storing the data in dataframes and exporting it to CSV
-    draft_df = pd.DataFrame(updated_draft_list, columns=['number_draft',
+    # Storing the data in dataframe and exporting it to CSV
+    draft_df = pd.DataFrame(updated_draft_list, columns=['year',
+                                                         'number_draft',
                                                          'name',
                                                          'number_of_games',
                                                          'total_minutes_played',
                                                          'total_points',
+
                                                          'total_rebounds',
                                                          'total_assists',
                                                          'minutes_per_game',
                                                          'points_per_game',
                                                          'rebounds_per_game',
+
                                                          'assists_per_game'
                                                          ''])
     print(draft_df)
-    draft_df.to_csv(name)
+    engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
+                           .format(user="root",
+                                   pw="pwdmysql",
+                                   db="basketball"))
+    draft_df.to_sql('players', con=engine, if_exists='append', chunksize=1000, index=False)
+    # draft_df.to_csv(name)
 
 print('\n\n\n----------\n\n\n')
 
 # Get global stats from main page
-print("\nRESULTS OF THE YEAR SO FAR")
-
-main = read_link(main_link)
-score_file = main.find("div", attrs={'class': "data_grid section_wrapper"})
-table_list = score_file.find_all('div', attrs={'class': "table_wrapper"})
-
-for table in table_list:
-    all_rows = table.find_all("tr", attrs={'class': 'full_table'})
-    break
-scores = []
-for row in all_rows:
-    print(row.a['title'] if row.a['title'] else "N/A", ' : ',
-          row.td.nextSibling.nextSibling.text if row.td.nextSibling.nextSibling.text else "N/A", ' wins, ',
-          row.td.nextSibling.nextSibling.nextSibling.text if row.td.nextSibling.nextSibling.nextSibling.text\
-else "N/A", 'losses')
-
-    # Adding data to file, and making sure it exists
-    info = [row.a['title'] if row.a['title'] else "N/A",
-            row.td.nextSibling.nextSibling.text if row.td.nextSibling.nextSibling.text else "N/A",
-            row.td.nextSibling.nextSibling.nextSibling.text if row.td.nextSibling.nextSibling.nextSibling.text \
-else "N/A"]
-    scores.append(info)
-scores_df = pd.DataFrame(scores, columns=['team', 'wins', 'losses'])
-scores_df.to_csv('scores.csv')
-
-print('\n\n LAST SCORES\n')
-scores = main.find('div', attrs={'class': 'game_summaries'})
-games = scores.find_all('table', attrs={'class': 'teams'})
-last_games = []
-for game in games:
-    loser = game.find('tr', attrs={'class': 'loser'}).find('a').text
-    score_loser = game.find('tr', attrs={'class': 'loser'}).find('td', attrs={'class': 'right'}).text
-    winner = game.find('tr', attrs={'class': 'winner'}).find('a').text
-    score_winner = game.find('tr', attrs={'class': 'winner'}).find('td', attrs={'class': 'right'}).text
-    print(winner, ' won to ', loser, ' with a score of ', score_winner, ' to ', score_loser)
-
-    # Adding data to a csv file
-    info_games = [winner if winner else "N/A",
-                  loser if loser else "N/A",
-                  score_winner if score_winner else "N/A",
-                  score_loser if score_loser else "N/A"]
-    last_games.append(info_games)
-last_games_df = pd.DataFrame(last_games, columns=['Winner', 'Loser', 'Score winner', 'Score loser'])
-last_games_df.to_csv('last_games.csv')
-
-print('\n\nCURRENT TRENDING PLAYERS\n')
-news = main.find('div', attrs={'id': 'current'})
-trending_players = news.find('div').nextSibling.find_all("a")
-for player in trending_players:
-    print(player.text)
-
-print('\n\nLAST NEWS - RUMORS\n')
-rumors = news.find('div').nextSibling.nextSibling.nextSibling.find_all('li')
-for rumor in rumors:
-    print(rumor.text, 'available at the link: ', rumor.a['href'])
+# print("\nRESULTS OF THE YEAR SO FAR")
+#
+# main = read_link(main_link)
+# score_file = main.find("div", attrs={'class': "data_grid section_wrapper"})
+# table_list = score_file.find_all('div', attrs={'class': "table_wrapper"})
+#
+# for table in table_list:
+#     all_rows = table.find_all("tr", attrs={'class': 'full_table'})
+#     break
+# scores = []
+# for row in all_rows:
+#     print(row.a['title'] if row.a['title'] else "N/A", ' : ',
+#           row.td.nextSibling.nextSibling.text if row.td.nextSibling.nextSibling.text else "N/A", ' wins, ',
+#           row.td.nextSibling.nextSibling.nextSibling.text if row.td.nextSibling.nextSibling.nextSibling.text\
+# else "N/A", 'losses')
+#
+#     # Adding data to file, and making sure it exists
+#     info = [row.a['title'] if row.a['title'] else "N/A",
+#             row.td.nextSibling.nextSibling.text if row.td.nextSibling.nextSibling.text else "N/A",
+#             row.td.nextSibling.nextSibling.nextSibling.text if row.td.nextSibling.nextSibling.nextSibling.text \
+# else "N/A"]
+#     scores.append(info)
+# scores_df = pd.DataFrame(scores, columns=['team', 'wins', 'losses'])
+# scores_df.to_csv('scores.csv')
+#
+# print('\n\n LAST SCORES\n')
+# scores = main.find('div', attrs={'class': 'game_summaries'})
+# games = scores.find_all('table', attrs={'class': 'teams'})
+# last_games = []
+# for game in games:
+#     loser = game.find('tr', attrs={'class': 'loser'}).find('a').text
+#     score_loser = game.find('tr', attrs={'class': 'loser'}).find('td', attrs={'class': 'right'}).text
+#     winner = game.find('tr', attrs={'class': 'winner'}).find('a').text
+#     score_winner = game.find('tr', attrs={'class': 'winner'}).find('td', attrs={'class': 'right'}).text
+#     print(winner, ' won to ', loser, ' with a score of ', score_winner, ' to ', score_loser)
+#
+#     # Adding data to a csv file
+#     info_games = [winner if winner else "N/A",
+#                   loser if loser else "N/A",
+#                   score_winner if score_winner else "N/A",
+#                   score_loser if score_loser else "N/A"]
+#     last_games.append(info_games)
+# last_games_df = pd.DataFrame(last_games, columns=['Winner', 'Loser', 'Score winner', 'Score loser'])
+# last_games_df.to_csv('last_games.csv')
+#
+# print('\n\nCURRENT TRENDING PLAYERS\n')
+# news = main.find('div', attrs={'id': 'current'})
+# trending_players = news.find('div').nextSibling.find_all("a")
+# for player in trending_players:
+#     print(player.text)
+#
+# print('\n\nLAST NEWS - RUMORS\n')
+# rumors = news.find('div').nextSibling.nextSibling.nextSibling.find_all('li')
+# for rumor in rumors:
+#     print(rumor.text, 'available at the link: ', rumor.a['href'])
