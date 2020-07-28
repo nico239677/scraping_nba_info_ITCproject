e432d2ffc69dcceb7e7ec738b7faf51f0543cfb7
nico239677 <nicolasmai0421@gmail.com>
2020-07-28 23:42:03 +0300

    Changed IP address

--- database_config.py
+++ database_config.py
@@ -5,7 +5,7 @@ import pymysql.cursors
 import logging
 
 
-connection = pymysql.connect(host='localhost',
+connection = pymysql.connect(host='172-31-15-65',
                              user='root',
                              password='pwdmysql',
                              charset='utf8mb4',
